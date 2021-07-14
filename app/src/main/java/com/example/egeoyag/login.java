c47438818ea3cc62faf9d9296d5f9ff558de548d
=vanheebin <=>
2021-07-14 12:24:46 +0900

    희빈 나의 모든것을 올립니다...

--- app/src/main/java/com/example/egeoyag/login.java
+++ app/src/main/java/com/example/egeoyag/login.java
@@ -2,13 +2,128 @@ package com.example.egeoyag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class login extends AppCompatActivity {
+    private EditText edt_email,edt_password;
+    private Button btn_login;
+    private RequestQueue queue;
+    private StringRequest stringRequest;
+    private ArrayList<MemberDTO> MemberList = new ArrayList<MemberDTO>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+
+        edt_email = findViewById(R.id.edt_email);
+        edt_password = findViewById(R.id.edt_password);
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+    }
+    public void sendRequest(){
+        queue = Volley.newRequestQueue(this);
+        String url ="http://172.17.96.1:3000/Login";
+
+        stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.v("성공1",response);
+                if(!response.equals("null")){
+                    Log.v("성공2",response);
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String id = jsonObject.getString("id");
+                        Log.v("성공3",id);
+                        String pw = jsonObject.getString("pw");
+                        Log.v("성공4",id);
+                        String name = jsonObject.getString("name");
+                        String tel = jsonObject.getString("tel");
+
+                        MemberDTO info = new MemberDTO(id,pw,name,tel);
+                        Gson gson = new Gson();
+
+                        String value = gson.toJson(info);
+
+                        Log.v("resultValue", value);
+                        PreferenceManager.setString(getApplicationContext(), "info", value);
+
+
+                        Intent intent = new Intent(getApplicationContext(),main.class);
+                        intent.putExtra("info",info);
+                        startActivity(intent);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }else {
+                    Toast.makeText(getApplicationContext(),"로그인에 실패 했습니다..",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                try {
+                    String utf8String = new String(response.data,"UTF-8");
+                    return Response.success(utf8String, HttpHeaderParser.parseCacheHeaders(response));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                return super.parseNetworkResponse(response);
+                //리스폰스 전에 먼저 여기로 온다.
+            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Server로 데이터를 보낼 시 넣어주는 곳
+                Map<String, String> paramas = new HashMap<String, String>();
+                paramas.put("user_id",edt_email.getText().toString());
+                paramas.put("user_pw",edt_password.getText().toString());
+                return paramas; // 네트워크 끼리 데이터를 주고 받는것을
+            }
+        };
+        queue.add(stringRequest);
     }
 }
--- app/src/main/java/com/example/egeoyag/subscription_information.java
+++ app/src/main/java/com/example/egeoyag/subscription_information.java
@@ -2,13 +2,147 @@ package com.example.egeoyag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class subscription_information extends AppCompatActivity {
+    private EditText edt_join_name,edt_join_pw,edt_join_pw_ch,edt_join_phone,edt_join_date;
+    private RadioButton rd_sex_m,rd_sex_w;
+    private Button btn_join;
+    private RequestQueue queue;
+    private StringRequest stringRequest;
+    private String user_id;
+    private RadioButton rd_re;
+
+    Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+            updateLabel();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscription_information);
+
+        Intent intent = getIntent();
+        user_id = intent.getStringExtra("user_id");// 인텐트 받아온값
+        Toast.makeText(getApplicationContext(),user_id,Toast.LENGTH_SHORT).show();
+
+        edt_join_name = findViewById(R.id.edt_join_name);
+        edt_join_pw = findViewById(R.id.edt_join_pw);
+        edt_join_pw_ch = findViewById(R.id.edt_join_pw_ch);
+        edt_join_phone = findViewById(R.id.edt_join_phone);
+        edt_join_date = findViewById(R.id.edt_join_date);
+
+        RadioGroup rd_s = findViewById(R.id.RadioGroup);
+        btn_join = findViewById(R.id.btn_join);
+
+        //서버에 보내는 버튼
+        btn_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int rd_id = rd_s.getCheckedRadioButtonId();
+                rd_re = findViewById(rd_id);
+                sendRequest();
+            }
+
+        });
+
+        //달력피커 출력
+        edt_join_date.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                new DatePickerDialog(subscription_information.this,myDatePicker,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+    }
+
+
+    //달력 포맷
+    private void updateLabel() {
+        String myFormat ="yyyy/MM/dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
+        edt_join_date.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private void sendRequest() {
+        queue = Volley.newRequestQueue(this);
+        String url ="http://172.17.96.1:3000/Join";
+
+        stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.v("resultValue",response);
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String value = jsonObject.getString("check");
+                    if (value.equals("true")) {
+
+                        Intent intent = new Intent(getApplicationContext(),login.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "회원가입에 실패 했습니다.", Toast.LENGTH_SHORT).show();
+                        edt_join_name.setText("");
+                        edt_join_pw.setText("");
+                        edt_join_pw_ch.setText("");
+                        edt_join_date.setText("");
+                        edt_join_phone.setText("");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }){
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> paramas = new HashMap<String, String>();
+                paramas.put("user_id",user_id); //인텐트 받은 값 서버에 올리기
+                paramas.put("user_pw",edt_join_pw_ch.getText().toString());
+                paramas.put("user_name",edt_join_name.getText().toString());
+                paramas.put("user_tel",edt_join_phone.getText().toString());
+                paramas.put("user_gender",rd_re.getText().toString());
+                paramas.put("user_birth",edt_join_date.getText().toString());
+                return paramas;
+            }
+
+        };
+        queue.add(stringRequest);
     }
 }
